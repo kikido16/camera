ee0b1dee3d2dff4d15da2f8993e46aee33bc8a87
kikido16 <837869599@qq.com>
2023-03-22 16:26:29 +0800

    2

---
+++ camera/camera/head.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <cmath> 
+#include <Windows.h>
+#include <thread>
+#include <condition_variable>
+#include <mutex>
+
+using namespace std;
+using namespace cv;
